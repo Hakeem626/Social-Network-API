6289a2fbc11dbca0317d46d6044474c1ffee412f
Hakeem <12151899q@gmail.com>
2023-09-09 12:34:16 -0700

    more changes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,23 +1,24 @@
 const router = require('express').Router();
 const {
-  getStudents,
-  getSingleStudent,
-  createStudent,
-  deleteStudent,
-  addAssignment,
-  removeAssignment,
-} = require('../../controllers/studentController');
+  getAllUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} = require('../../controllers/userController');
 
 // /api/students
-router.route('/').get(getStudents).post(createStudent);
+router.route('/').get(getAllUsers).post(createUser);
 
-// /api/students/:studentId
-router.route('/:studentId').get(getSingleStudent).delete(deleteStudent);
+// /api/students/:userId
+router.route('/:userId').get(getUser).delete(deleteUser).put(updateUser);
 
-// /api/students/:studentId/assignments
-router.route('/:studentId/assignments').post(addAssignment);
+// /api/students/:userId/assignments
+router.route('/:userId/friends').post(addFriend);
 
-// /api/students/:studentId/assignments/:assignmentId
-router.route('/:studentId/assignments/:assignmentId').delete(removeAssignment);
+// /api/students/:userId/assignments/:assignmentId
+router.route('/:userId/friends/:friendId').delete(removeFriend);
 
 module.exports = router;
