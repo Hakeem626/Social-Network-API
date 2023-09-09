4202357faf5deb520e5dad34dae3a61ae14008f5
Hakeem <12151899q@gmail.com>
2023-09-09 10:32:08 -0700

    models updated

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require();
+// Schema to create Student model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/\w+@\w{2,3}+/, "Invalid email address"],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "thought"
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "user"
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtual: true,
+    },
+  }
+);
+
+userSchema.virtual("friendCount").get(function (){
+  return this.friends.length
+});
+
+const Student = model('user', userSchema);
+
+module.exports = User;
